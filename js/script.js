7d6a97a401fdef1fccdcf5701ca0ac458fe14b31
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2023-02-14 10:17:56 +0100

    Big Data

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
-let question = document.querySelectorAll(".question");
+var splide = new Splide("#main-slider", {
+  width: 600,
+  height: 300,
+  pagination: false,
+  cover: true
+});
 
-question.forEach(question => {
-  question.addEventListener("click", event => {
-    const active = document.querySelector(".question.active");
-    if(active && active !== question ) {
-      active.classList.toggle("active");
-      active.nextElementSibling.style.maxHeight = 0;
+var thumbnails = document.getElementsByClassName("thumbnail");
+var current;
+
+for (var i = 0; i < thumbnails.length; i++) {
+  initThumbnail(thumbnails[i], i);
+}
+
+function initThumbnail(thumbnail, index) {
+  thumbnail.addEventListener("click", function () {
+    splide.go(index);
+  });
+}
+
+splide.on("mounted move", function () {
+  var thumbnail = thumbnails[splide.index];
+
+  if (thumbnail) {
+    if (current) {
+      current.classList.remove("is-active");
     }
-    question.classList.toggle("active");
-    const answer = question.nextElementSibling;
-    if(question.classList.contains("active")){
-      answer.style.maxHeight = answer.scrollHeight + "px";
-    } else {
-      answer.style.maxHeight = 0;
-    }
-  })
-})
+
+    thumbnail.classList.add("is-active");
+    current = thumbnail;
+  }
+});
+
+splide.mount();
